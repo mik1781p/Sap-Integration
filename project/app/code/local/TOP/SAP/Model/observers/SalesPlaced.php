09adf9ceaf238ca77c63813be79077c3a1c97e52
Lorenzo Calamandrei <lorenzo.calamandrei@thinkopen.it>
2017-10-24 12:26:24 +0200

    TOP SAP Module
    Structure done for the project.

--- project/app/code/local/TOP/SAP/Model/observers/SalesPlaced.php
+++ project/app/code/local/TOP/SAP/Model/observers/SalesPlaced.php
@@ -1,10 +1,45 @@
 <?php
 
+/**
+ * Class TOP_SAP_Model_observers_SalesPlaced
+ */
+
+/**
+ * Class TOP_SAP_Model_observers_SalesPlaced
+ * Observer on sales_order_place_after
+ */
+
 class TOP_SAP_Model_observers_SalesPlaced extends Mage_Core_Model_Session_Abstract
 {
     public function orderSAP(Varien_Event_Observer $event_Observer)
     {
-        Mage::log($event_Observer->getOrder()->getData());
-        //TODO do it!
+        /*
+         * Example data
+            2017-10-24T10:23:19+00:00 DEBUG (7): Street:
+            2017-10-24T10:23:19+00:00 DEBUG (7): Array
+            (
+                [0] => Via di test
+            )
+
+            2017-10-24T10:23:19+00:00 DEBUG (7): City:
+            2017-10-24T10:23:19+00:00 DEBUG (7): Città test
+
+            2017-10-24T10:23:19+00:00 DEBUG (7): PostCode:
+            2017-10-24T10:23:19+00:00 DEBUG (7): Ancora test
+
+            2017-10-24T10:23:19+00:00 DEBUG (7): Country:
+            2017-10-24T10:23:19+00:00 DEBUG (7): IT
+
+         */
+        Mage::log('Street: ');
+        Mage::log($event_Observer->getOrder()->getShippingAddress()->getStreet());
+        Mage::log('City: ');
+        Mage::log($event_Observer->getOrder()->getShippingAddress()->getCity());
+        Mage::log('PostCode: ');
+        Mage::log($event_Observer->getOrder()->getShippingAddress()->getPostcode());
+        Mage::log('Country: ');
+        Mage::log($event_Observer->getOrder()->getShippingAddress()->getCountryId());
+        die;
+        //TODO cURL it!
     }
 }
